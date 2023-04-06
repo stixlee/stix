2eb58b35bf341eaddf21d83790667e76a0f83197
Stix Lee <michael.lee.1983@icloud.com>
2023-04-06 12:53:22 -0600

    Initial Commit

---
+++ Stix/Stix/StixApp.swift
@@ -0,0 +1,17 @@
+//
+//  StixApp.swift
+//  Stix
+//
+//  Created by Michael Lee on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct StixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
